1570aaf49cfb5c6064b0582d90cbefc5cb29c96f
Jarret Moses <jarret.moses@gmail.com>
2014-09-03 13:57:23 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	before_save { email.downcase! } #callback that (before save method invokes) will force email to be downcased
+	has_many :microposts, dependent: :destroy
+	before_save { email.downcase! } #callback that (before save method invokes) will force email to be downcased to ensure uniqueness
 	before_create :create_remember_token
 
 	validates :name,  presence: true, length: { maximum: 50 }
@@ -17,6 +18,11 @@ class User < ActiveRecord::Base
 	def User.digest(token)
 		Digest::SHA1.hexdigest(token.to_s)
 	end
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
 	private
 		def create_remember_token
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   
   root 'static_pages#home'
+
+  # get "/profile/:id",   to: 'users#show',           as: :profile
 
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
